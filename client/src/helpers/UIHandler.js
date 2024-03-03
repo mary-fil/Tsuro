5a854c83dfac316290989d644bff07a5fab88f2a
Maria Filocha <maritka10@gmail.com>
2024-03-03 10:17:38 +0100

    Add handlers and assets.

--- client/src/helpers/UIHandler.js
+++ client/src/helpers/UIHandler.js
@@ -1,15 +1,43 @@
 //handling all user interface stuff
+//dependency injection
+
+import ZoneHandler from "./ZoneHandler";
 
 export class UIHandler {
 
     constructor (scene)
     {
+        this.zoneHandler = new ZoneHandler(scene);
+
+        this.buildZones = () => {
+            scene.dropZone = this.zoneHandler.renderZone(470, 500);
+            this.zoneHandler.renderOutline(scene.dropZone);
+        }
+
+        this.buildPlayerAreas = () => {
+
+            //player
+            scene.playerHandArea = scene.add.rectangle(470, 860, 850, 230);
+            scene.playerHandArea.setStrokeStyle(4, 0xff69b4);
+            scene.playerDeckArea = scene.add.rectangle(1000, 860, 155, 215);
+            scene.playerDeckArea.setStrokeStyle(3, 0x00ffff);
+
+            //opponent
+            scene.opponentHandArea = scene.add.rectangle(470, 135, 850, 230);
+            scene.opponentHandArea.setStrokeStyle(4, 0xff69b4);
+            scene.opponentDeckArea = scene.add.rectangle(1000, 135, 155, 215);
+            scene.opponentDeckArea.setStrokeStyle(3, 0x00ffff);
+        }
+
         this.buildGameText = () => {
             scene.dealCards = scene.add.text(960, 445, "Deal cards").setFontSize(14).setFontFamily("Trebuchet MS");
         }
 
         this.buildUI = () => {
+            this.buildZones();
+            this.buildPlayerAreas();
             this.buildGameText();
+            
         }
     }
 }
--- client/src/scenes/Boot.js
+++ client/src/scenes/Boot.js
@@ -1,5 +1,10 @@
 import { Scene } from 'phaser';
 import { UIHandler } from '../helpers/UIHandler';
+import InteractiveHandler from '../helpers/InteractiveHandler';
+import CardHandler from '../helpers/CardHandler';
+import DeckHandler from '../helpers/DeckHandler';
+import SocketHandler from '../helpers/SocketHandler';
+import GameHandler from '../helpers/GameHandler';
 
 export class Boot extends Scene
 {
@@ -13,13 +18,26 @@ export class Boot extends Scene
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
-        this.load.image('background', 'assets/bg.png');
+        //this.load.image('background', 'assets/bg.png');
+        this.load.image('tile1', 'assets/tile1.png');
+        this.load.image('tile2', 'assets/tile2.png');
+        this.load.image('tile3', 'assets/tile3.png');
+        this.load.image('tile4', 'assets/tile4.png');
+        this.load.image('tile5', 'assets/tile5.png');
+        this.load.image('tile6', 'assets/tile6.png');
     }
 
     create ()
     {
+        this.CardHandler = new CardHandler();
+        this.DeckHandler = new DeckHandler();
+        this.GameHandler = new GameHandler();
+        this.SocketHandler = new SocketHandler();
+        
         this.UIHandler = new UIHandler(this);
         this.UIHandler.buildUI();
+
+        this.InteractiveHandler = new InteractiveHandler();
         //this.scene.start('Preloader');
     }
 
